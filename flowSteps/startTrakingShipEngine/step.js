e3ba44d5b3ff2b5c76360075d2c30af7f7263138
Alejandro Greggio <agreggio@slingr.io>
2023-04-26 17:44:27 -0300

    #3 Standarized shipengine endpoint

---
+++ flowSteps/startTrakingShipEngine/step.js
@@ -0,0 +1,16 @@
+step.trakingShipEngine = function (inputs) {
+
+	var inputsLogic = {
+		params:{
+			carrier_code:inputs.carrierCode || "",
+			tracking_number:inputs.trackingNumber || ""
+		} || []
+	};
+
+	var options = {
+		path: "/tracking/start",
+		params: inputsLogic.params,
+	}
+
+	return endpoint._post(options);
+}
---
+++ flowSteps/trakingShipEngine/step.js
@@ -0,0 +1,16 @@
+step.trakingShipEngine = function (inputs) {
+
+	var inputsLogic = {
+		params:{
+			carrier_code:inputs.carrierCode || "",
+			tracking_number:inputs.trackingNumber || ""
+		} || []
+	};
+
+	var options = {
+		path: "/tracking",
+		params: inputsLogic.params,
+	}
+
+	return endpoint._get(options);
+}
